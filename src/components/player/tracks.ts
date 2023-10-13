f418284555a2e651ad0b7879478c6e423ccdf5cd
Khanh Luu <luukhanh96@gmail.com>
2023-10-14 00:55:15 +0700

    update metadata

---
+++ src/components/player/tracks.ts
@@ -0,0 +1,68 @@
+export const tracks = [
+	{
+	  track: '/at-my-worst.mp3',
+	  title: 'At my worst',
+	  url: '/at-my-worst.jpeg',
+	  author: 'Pink Sweat$',
+	},
+	{
+	  track: '/can-i-have-this-dance.mp3',
+	  title: 'Can I have this dance',
+	  url: '/can-i-have-this-dance.jpeg',
+	  author: 'Vanessa Hudgens, Zac Efron',
+	},
+	{
+	  track: '/dua-em-ve-nha.mp3',
+	  title: 'Đưa em về nhà',
+	  url: '/dua-em-ve-nha.jpeg',
+	  author: 'Grey D',
+	},
+	{
+	  track: '/i-dont-want-miss-a-thing.mp3',
+	  title: "I don't want miss a thing",
+	  url: '/i-dont-want-miss-a-thing.jpeg',
+	  author: 'Aerosmith (Boyce Avenue ft. Jennel Garcia cover)',
+	},
+	{
+	  track: '/la-vien-rose.mp3',
+	  title: 'La vien rose',
+	  url: '/la-vien-rose.jpeg',
+	  author: 'Daniela Andrade',
+	},
+	{
+	  track: '/love-me-like-you.mp3',
+	  title: 'Love me like you',
+	  url: '/love-me-like-you.jpeg',
+	  author: 'Little mix',
+	},
+	{
+	  track: '/lovely.mp3',
+	  title: '사랑스러워',
+	  url: '/lovely.jpeg',
+	  author: 'Kim Jong Kook',
+	},
+	{
+	  track: '/ngo-cham.mp3',
+	  title: 'Ngõ chạm',
+	  url: '/ngo-cham.jpeg',
+	  author: 'Big Daddy x Emily',
+	},
+	{
+	  track: '/no-place.mp3',
+	  title: 'No place',
+	  url: '/no-place.jpeg',
+	  author: 'Backstreet boys',
+	},
+	{
+	  track: '/perfect.mp3',
+	  title: 'Perfect',
+	  url: '/perfect.jpeg',
+	  author: 'Ed Sheeran',
+	},
+	{
+	  track: '/photograph.mp3',
+	  title: 'Photograph',
+	  url: '/photograph.jpeg',
+	  author: 'Ed Sheeran',
+	},
+ ];
